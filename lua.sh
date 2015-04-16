73b29500522ba047e2c5d0b72e2ffa78fad46e13
Hallison Batista <hallisonbatista@gmail.com>
2015-04-16 15:08:13 -0400

    Fixes in the root directory of Lua installation

--- lua.sh
+++ lua.sh
@@ -3,7 +3,7 @@ desc="Default/official Lua interpreter."
 
 luavm_patch() {
   cp $LUA_SOURCE/src/luaconf.h $LUA_SOURCE/src/luaconf.h.orig
-  sed "s|^\(#define LUA_ROOT\)\(.*\)$|\1 \"$LUA_HOME\"|" $LUA_SOURCE/src/luaconf.h.orig > $LUA_SOURCE/src/luaconf.h
+  sed "s|^\(#define LUA_ROOT\)\(.*\)$|\1 \"$LUA_HOME/\"|" $LUA_SOURCE/src/luaconf.h.orig > $LUA_SOURCE/src/luaconf.h
 
   cp $LUA_SOURCE/Makefile $LUA_SOURCE/Makefile.orig
   sed "s|^\(INSTALL_TOP= \)\(.*\)$|\1$LUA_HOME|" $LUA_SOURCE/Makefile.orig > $LUA_SOURCE/Makefile
